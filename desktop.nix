9443e6bc9ed7b0b24ee823f9e9f47837ad802fac
Emma Benkart <emmabenkart@gmail.com>
2025-08-23 09:38:22 -0700

    Configure sway

--- desktop.nix
+++ desktop.nix
@@ -1,6 +1,5 @@
-{
-  pkgs,
-  ...
+{ pkgs
+, ...
 }:
 {
   services = {
@@ -22,12 +21,68 @@
       wrapperFeatures.gtk = true;
       config = {
         modifier = "Mod4";
+        menu = "${pkgs.wofi}/bin/wofi --show drun";
         terminal = "${pkgs.wezterm}/bin/wezterm";
+        bars = [ ];
+        output = {
+          DP-3 = {
+            pos = "0 0";
+          };
+          DP-1 = {
+            pos = "1920 0";
+          };
+          DP-2 = {
+            pos = "4480 0";
+          };
+        };
+        workspaceOutputAssign = [
+          {
+            workspace = "1";
+            output = "DP-3";
+          }
+          {
+            workspace = "2";
+            output = "DP-1";
+          }
+          {
+            workspace = "3";
+            output = "DP-2";
+          }
+          {
+            workspace = "4";
+            output = "DP-1";
+          }
+          {
+            workspace = "5";
+            output = "DP-1";
+          }
+          {
+            workspace = "6";
+            output = "DP-1";
+          }
+          {
+            workspace = "7";
+            output = "DP-1";
+          }
+          {
+            workspace = "8";
+            output = "DP-1";
+          }
+          {
+            workspace = "9";
+            output = "DP-1";
+          }
+          {
+            workspace = "0";
+            output = "DP-1";
+          }
+        ];
       };
     };
     programs = {
       waybar = {
         enable = true;
+        systemd.enable = true;
         settings = {
           mainBar = {
             modules-left = [ "sway/workspaces" ];
