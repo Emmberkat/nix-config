d1f010facd3c1c4aa1d7ac882ccebfdfa50bbd21
Emma Benkart <emmabenkart@gmail.com>
2025-09-06 14:25:32 -0700

    Refactor home manager config and enable syncthing

---
+++ emmberkat/git.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
+      userName = "Emma Benkart";
+      userEmail = "emmabenkart@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+  };
+}
---
+++ emmberkat/home.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  pkgs,
+  agenix,
+  ...
+}:
+{
+
+  imports = [
+    ./backups.nix
+    ./browser.nix
+    ./desktop.nix
+    ./git.nix
+    ./neovim.nix
+    ./zsh.nix
+    agenix.homeManagerModules.default
+  ];
+
+  home.stateVersion = "22.11";
+  nixpkgs.config.allowUnfree = true;
+
+}
