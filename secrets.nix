05f969b1ff7a628943ab44766e638dbba1f32d43
Emma Benkart <emmabenkart@gmail.com>
2025-09-09 17:35:59 +0000

    Refactor secrets
    This commit moves publickeys into their own directory for improved
    readability when managing secrets.

---
+++ secrets.nix
@@ -0,0 +1,41 @@
+let
+  keysDir = ./publickeys;
+  keyFiles = builtins.readDir keysDir;
+  keys = builtins.mapAttrs (name: value: builtins.readFile (keysDir + "/${name}")) keyFiles;
+in
+{
+  "user/emmberkat/secrets/syncthing/key.age" = {
+    publicKeys = [
+      keys.emmberkat
+      keys.crystal
+    ];
+    armor = true;
+  };
+  "user/emmberkat/secrets/syncthing/cert.age" = {
+    publicKeys = [
+      keys.emmberkat
+      keys.crystal
+    ];
+    armor = true;
+  };
+  "user/emmberkat/secrets/restic/environment.age" = {
+    publicKeys = [
+      keys.emmberkat
+      keys.crystal
+    ];
+    armor = true;
+  };
+  "user/emmberkat/secrets/restic/password.age" = {
+    publicKeys = [
+      keys.emmberkat
+      keys.crystal
+    ];
+    armor = true;
+  };
+  "system/catalyst/secrets/ddclient/password.age" = {
+    publicKeys = [
+      keys.catalyst
+    ];
+    armor = true;
+  };
+}
