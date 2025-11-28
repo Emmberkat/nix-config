235192ec51a5e27aeb250be3c7ff296950db3b40
Em Benkart <746524+Emmberkat@users.noreply.github.com>
2025-11-28 10:37:10 -0800

    Remove power management targets from default.nix
    Removed sleep, suspend, hibernate, and hybrid-sleep targets from configuration.

--- system/crystal/default.nix
+++ system/crystal/default.nix
@@ -37,12 +37,6 @@
         };
       };
     };
-    targets = {
-      sleep.enable = false;
-      suspend.enable = false;
-      hibernate.enable = false;
-      hybrid-sleep.enable = false;
-    };
   };
 
   hardware = {
