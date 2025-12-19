720abe34394b286d54132ffba38eabc33889f729
Em Benkart <746524+Emmberkat@users.noreply.github.com>
2025-12-18 20:28:09 -0800

    Add swapfile configuration to default.nix

--- system/catalyst/default.nix
+++ system/catalyst/default.nix
@@ -185,6 +185,7 @@
 
   swapDevices = [
     { device = "/dev/disk/by-uuid/0a97e7b1-5e81-4833-8cce-eb50a945b265"; }
+    { device = "/swapfile"; size = 65536; }
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
