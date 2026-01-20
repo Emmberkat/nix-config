a22b07d16bf108f9d6bdd022f59bbfa959f00206
Em Benkart <746524+Emmberkat@users.noreply.github.com>
2026-01-19 22:04:53 -0800

    Fix issue with latest treesitter
    Removed configuration for nvim-treesitter plugin. The plugin has been reworked and the defaults should work fine. nvim-treesitter.configs no longer exists.

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -60,14 +60,6 @@
         {
           plugin = nvim-treesitter;
           type = "lua";
-          config = ''
-            require'nvim-treesitter.configs'.setup {
-              highlight = {
-                enable = true,
-              },
-              additional_vim_regex_highlighting = false,
-            }
-          '';
         }
         {
           plugin = telescope-nvim;
