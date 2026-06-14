f18f1885581077c53be0632f0ade0d0dd2962aab
Emma Benkart <emmabenkart@gmail.com>
2026-06-14 21:28:47 +0000

    Lint with statix

--- system/catalyst/containers/homeassistant.nix
+++ system/catalyst/containers/homeassistant.nix
@@ -4,85 +4,88 @@ let
   swsPort = 8787;
 in
 {
-  services.nginx.virtualHosts."home.emmberkat.com" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://localhost:${toString port}";
-      proxyWebsockets = true;
-    };
-  };
-  networking.firewall.allowedTCPPorts = [ swsPort ];
-  services.home-assistant = {
-    enable = true;
-    openFirewall = true;
-    extraComponents = [
-      "esphome"
-      "zha"
-      "zwave_js"
-      "isal"
-      "usb"
-      "tts"
-      "stt"
-      "whisper"
-      "piper"
-      "wyoming"
-      "ollama"
-      "imap"
-      "roborock"
-      "met"
-      "shopping_list"
-      "scl"
-      "tile"
-      "webostv"
-      "waze_travel_time"
-      "opower"
-      "immich"
-      "habitica"
-      "steam_online"
-      "google"
-      "google_mail"
-      "ring"
-      "tesla_fleet"
-      "jellyfin"
-      "litterrobot"
-      "onvif"
-    ];
-    configDir = "/mnt/hass";
-    config = {
-      default_config = { };
-      http = {
-        server_port = port;
-        use_x_forwarded_for = true;
-        trusted_proxies = [
-          "127.0.0.1"
-          "::1"
-        ];
+  services = {
+    nginx.virtualHosts."home.emmberkat.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:${toString port}";
+        proxyWebsockets = true;
       };
-      "automation ui" = "!include automations.yaml";
-      "scene ui" = "!include scenes.yaml";
-      "script ui" = "!include scripts.yaml";
     };
-  };
-
-  services.wyoming = {
-    piper.servers.hass = {
+    home-assistant = {
       enable = true;
-      uri = "tcp://0.0.0.0:10200";
-      voice = "en_US-danny-low";
-    };
-
-    faster-whisper = {
-      package = pkgs.pkgsCuda.wyoming-faster-whisper;
-      servers.hass = {
-        enable = true;
-        uri = "tcp://0.0.0.0:10300";
-        language = "en";
-        model = "medium.en";
-        device = "cuda";
+      openFirewall = true;
+      extraComponents = [
+        "esphome"
+        "zha"
+        "zwave_js"
+        "isal"
+        "usb"
+        "tts"
+        "stt"
+        "whisper"
+        "piper"
+        "wyoming"
+        "ollama"
+        "imap"
+        "roborock"
+        "met"
+        "shopping_list"
+        "scl"
+        "tile"
+        "webostv"
+        "waze_travel_time"
+        "opower"
+        "immich"
+        "habitica"
+        "steam_online"
+        "google"
+        "google_mail"
+        "ring"
+        "tesla_fleet"
+        "jellyfin"
+        "litterrobot"
+        "onvif"
+      ];
+      configDir = "/mnt/hass";
+      config = {
+        default_config = { };
+        http = {
+          server_port = port;
+          use_x_forwarded_for = true;
+          trusted_proxies = [
+            "127.0.0.1"
+            "::1"
+          ];
+        };
+        "automation ui" = "!include automations.yaml";
+        "scene ui" = "!include scenes.yaml";
+        "script ui" = "!include scripts.yaml";
       };
     };
 
+    wyoming = {
+      piper.servers.hass = {
+        enable = true;
+        uri = "tcp://0.0.0.0:10200";
+        voice = "en_US-danny-low";
+      };
+
+      faster-whisper = {
+        package = pkgs.pkgsCuda.wyoming-faster-whisper;
+        servers.hass = {
+          enable = true;
+          uri = "tcp://0.0.0.0:10300";
+          language = "en";
+          model = "medium.en";
+          device = "cuda";
+        };
+      };
+
+    };
   };
+
+  networking.firewall.allowedTCPPorts = [ swsPort ];
 
 }
--- system/catalyst/containers/llm.nix
+++ system/catalyst/containers/llm.nix
@@ -4,54 +4,57 @@ let
   ollamaPort = 8041;
 in
 {
-  services.nginx.virtualHosts = {
-    "llm.emmberkat.com" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://localhost:${toString openwebuiPort}";
-        proxyWebsockets = true;
+  services = {
+    nginx.virtualHosts = {
+      "llm.emmberkat.com" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://localhost:${toString openwebuiPort}";
+          proxyWebsockets = true;
+        };
+      };
+      "ollama.emmberkat.com" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://localhost:${toString ollamaPort}";
+          proxyWebsockets = true;
+          extraConfig = ''
+            allow 127.0.0.1/32;
+            allow 10.0.0.0/8;
+            deny all;
+          '';
+        };
       };
     };
-    "ollama.emmberkat.com" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://localhost:${toString ollamaPort}";
-        proxyWebsockets = true;
-        extraConfig = ''
-          allow 127.0.0.1/32;
-          allow 10.0.0.0/8;
-          deny all;
-        '';
-      };
-    };
-  };
 
-  services.open-webui = {
-    enable = true;
-    port = openwebuiPort;
-    environment = {
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
-      OLLAMA_API_BASE_URL = "http://localhost:${toString ollamaPort}";
+    open-webui = {
+      enable = true;
+      port = openwebuiPort;
+      environment = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        OLLAMA_API_BASE_URL = "http://localhost:${toString ollamaPort}";
+      };
     };
-  };
-  services.ollama = {
-    enable = true;
-    port = ollamaPort;
-    user = "ollama";
-    home = "/mnt/ollama";
-    package = pkgs.ollama-cuda;
-    openFirewall = true;
-    environmentVariables = {
-      OLLAMA_ORIGINS = "*";
+
+    ollama = {
+      enable = true;
+      port = ollamaPort;
+      user = "ollama";
+      home = "/mnt/ollama";
+      package = pkgs.ollama-cuda;
+      openFirewall = true;
+      environmentVariables = {
+        OLLAMA_ORIGINS = "*";
+      };
+      loadModels = [
+        "qwen3.5:4b"
+        "qwen3.5:9b"
+      ];
     };
-    loadModels = [
-      "qwen3.5:4b"
-      "qwen3.5:9b"
-    ];
   };
 
 }
