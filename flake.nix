9221cc12029b47af163d2f251281f98549a1a103
Em Benkart <746524+Emmberkat@users.noreply.github.com>
2026-07-17 06:05:40 -0700

    Switch to unstable nixpkgs
    Despite the name, unstable is actually more stable than master

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     ];
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     nix-minecraft = {
       url = "github:Infinidoge/nix-minecraft";
