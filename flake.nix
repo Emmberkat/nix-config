de24dd88730256ba52811cf77f1de1ecf757a801
Emma Benkart <emmabenkart@gmail.com>
2026-08-05 14:33:58 +0000

    Add additional binary cache

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos-cuda.org"
+      "https://nix.emmberkat.com"
     ];
     extra-trusted-public-keys = [
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
